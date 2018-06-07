99c26f30e0146d2a297e8267d30a34f5010b7df6
shravanvis <shravanvishwakarma5@gmail.com>
2018-06-07 21:50:50 +0530

    First commit

---
+++ object01.js
@@ -0,0 +1,15 @@
+var person = new Object();
+    person.Firstname = 'shravan'
+    person.Lastname = 'kumar'
+    person.age = '20'
+
+    console.log(person)
+    console.log(typeof person)
+
+    for(var i in person){
+        console.log(i+ ' ' +person[i])
+    }
+
+    console.log(person.Firstname)
+    console.log(person.Lastname)
+    console.log(person.age)
